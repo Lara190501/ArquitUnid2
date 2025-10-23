41e4a0927e6e9e60efcbe81ccd1504490408e154
unknown <alunoD14@acad.unifor.local>
2025-10-23 18:22:50 -0300

    add ativ

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+    
+        ChannelFactory tcpFactory = new TCPFactory();
+        Channel tcp = tcpFactory.create();
+        tcp.getMessage();
+
+        
+        ChannelFactory udpFactory = new UDPFactory();
+        Channel udp = udpFactory.create();
+        udp.getMessage();
+    }
+}
---
+++ Teste.java
@@ -0,0 +1,35 @@
+
+interface Channel {
+    void getMessage();
+}
+
+
+class TCPChannel implements Channel {
+    public void getMessage() {
+        System.out.println("Envia mensagem TCP");
+    }
+}
+
+class UDPChannel implements Channel {
+    public void getMessage() {
+        System.out.println("Envia mensagem UDP");
+    }
+}
+
+
+abstract class ChannelFactory {
+    public abstract Channel create();
+}
+
+
+class TCPFactory extends ChannelFactory {
+    public Channel create() {
+        return new TCPChannel();
+    }
+}
+
+class UDPFactory extends ChannelFactory {
+    public Channel create() {
+        return new UDPChannel();
+    }
+}
